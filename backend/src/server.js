da5b155d7aaae737e2bca0f9f5101b00a3647131
Dominion Okori <domweb141@gmail.com>
2026-04-14 23:33:48 +0100

    fix: ensure backend dependencies installed

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,12 +25,14 @@ app.use("/api/messages", messageRoutes)
 
 
 //Make ready for deployment.
+/*
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.use((req, res)=>{
         res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
     });
 }
+*/
 
 //Listen for request
 app.listen(port, () =>{ console.log('Chit-chat server running now...')});
