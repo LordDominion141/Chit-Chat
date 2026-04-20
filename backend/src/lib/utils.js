4a235059db1b5b718d47ed27eaee1902668f81d5
Dominion Okori <domweb141@gmail.com>
2026-04-20 12:23:42 +0100

    fixed other things

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true, // keeps it safe from JS
         sameSite: "Lax", // "Lax" is much better for testing than "Strict"
-        secure: false,   // CRITICAL: Set to false so it works over http://
+        secure: ENV.NODE_ENV === "production",   // CRITICAL: Set to false so it works over http://
     });
 
     return token;
