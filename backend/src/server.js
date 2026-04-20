cd8186bc4ad0150dfc51b47dbf510e3003982990
Dominion Okori <domweb141@gmail.com>
2026-04-20 12:23:42 +0100

    fixed other things

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,8 +21,7 @@ const PORT = ENV.PORT || 3000;
 //Initialize our API URL, and use our requests from the requests file.
 
 
-app.use(express.json())
-
+app.use(express.json({ limit: '5mb' }));  // Added size limit
 app.use(cookieParser())
 
 app.use((req, res, next) => {
